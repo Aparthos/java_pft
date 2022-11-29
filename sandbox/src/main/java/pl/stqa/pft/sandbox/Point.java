9c6fa7272517792e1d739322fa5da3829432e895
Aparthos <kamilmal7wp.pl@wp.pl>
2022-11-29 17:43:03 +0100

    Zadanie domowe Kamil Malinowski

---
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/Point.java
@@ -0,0 +1,45 @@
+package pl.stqa.pft.sandbox;
+
+public class Point {
+
+  private double x;
+  private double y;
+
+  Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public double getDistance(Point p) {
+
+    return Math.sqrt((this.getX() - p.getX()) * (this.getX() - p.getX())
+            + (this.getY() - p.getY()) * (this.getY() - p.getY()));
+  }
+
+  public static void main(String[] args) {
+
+    Point px = new Point(3, 3);
+
+    Point py = new Point(8, 8);
+
+    double distance = px.getDistance(py);
+
+    System.out.println("odległość pomiędzy punktami px oraz py wynosi " + distance);
+  }
+
+
+}
+
+
+
+
+
+
