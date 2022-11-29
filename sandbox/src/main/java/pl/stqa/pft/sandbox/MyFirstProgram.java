d5541153795800cf6083912d7651f99018709e37
Aparthos <kamilmal7wp.pl@wp.pl>
2022-11-29 17:43:19 +0100

    Zmiany

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -10,12 +10,16 @@ public class MyFirstProgram {
     Square s = new Square(5);
     System.out.println("powierzchnia kwadratu o boku " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(4,6);
+    Rectangle r = new Rectangle(5,6);
     System.out.println("powierzchnia prostokąta o bokach " + r.a + " i " + r.b + "=" + r.area());
+
+
   }
 
   public static void hello (String somebody) {
       System.out.println("Hello, " + somebody + "!");
   }
+            }
 
-}
+
+
