2913f2ffa5c043db6275616221b8637abab9d9d9
Aparthos <kamilmal7wp.pl@wp.pl>
2023-01-04 20:00:34 +0100

    Analiza opcji wiersza poleceń przy uruchomieniu generatora danych

---
+++ adressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,91 @@
+package pl.stqa.pft.adressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import pl.stqa.pft.adressbook.model.ContactData;
+import pl.stqa.pft.adressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
+
+
+
+  public static void main (String[] args) throws IOException {
+
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+
+
+    } catch (ParameterException ex) {
+
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+
+
+
+
+
+
+  }
+
+  private void run() throws IOException {
+
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+
+  }
+
+
+    private void save (List<ContactData> contacts, File file) throws IOException {
+
+
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s\n", contact.getName(), contact.getSurname()));
+    }
+  writer.close();
+
+  }
+
+  private  List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withName(String.format("Kamil %s", i))
+              .withSurname(String.format("Surname %s", i)));
+
+
+  }
+
+    return contacts;
+
+
+  }
+
+
+}
+
+
+
+
+
