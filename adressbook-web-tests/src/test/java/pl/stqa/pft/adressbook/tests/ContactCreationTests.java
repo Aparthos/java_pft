07e820a6bbac5e64a5b69ed69871e3e8bb14e6ae
Aparthos <kamilmal7wp.pl@wp.pl>
2023-01-05 18:09:38 +0100

    Ładowanie danych testowych i parametryzacja metod

--- adressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/tests/ContactCreationTests.java
@@ -3,24 +3,47 @@ package pl.stqa.pft.adressbook.tests;
 import org.testng.annotations.*;
 import pl.stqa.pft.adressbook.model.ContactData;
 import pl.stqa.pft.adressbook.model.Contacts;
+import pl.stqa.pft.adressbook.model.GroupData;
 
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactCreationTests extends TestBaseC{
+public class ContactCreationTests extends TestBaseC {
 
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
+    String line = reader.readLine();
+
+    while (line != null) {
+
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withName(split[0]).withSurname(split[1])});
+      line = reader.readLine();
+
+
+    }
+
+  return list.iterator();
+
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
 
 
     app.goToC().Home();
     Contacts before = app.contact().all();
 
-    ContactData contact = new ContactData().withName("Kamil").withSurname("Malinowski");
 
     File photo = new File("src/test/resources/images.jfif");
     app.contact().create(new ContactData().withName("Kamil").withSurname("Malinowski").withPhoto(photo), false);
@@ -32,9 +55,9 @@ public class ContactCreationTests extends TestBaseC{
 
 
   }
-
-
 }
+
+
 
 
 
--- adressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -6,29 +6,63 @@ import pl.stqa.pft.adressbook.model.GroupData;
 import pl.stqa.pft.adressbook.model.Groups;
 
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
 
+  @DataProvider
+  public Iterator <Object[]> validGroups() throws IOException {
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName ("test2");
-    app.group().create(group);
-    Groups after = app.group().all();
-    assertThat (app.group().count(), equalTo(before.size() + 1));
-    assertThat(after, equalTo(
-            before.withAdded(group.withtId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+
+    while (line != null) {
+
+      String[] split = line.split(";");
+      list.add(new Object[] { new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+
+
+
+
+    }
+
+
+
+
+    return list.iterator();
+  }
+
+
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
+
+
+
+
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      Groups after = app.group().all();
+      assertThat(app.group().count(), equalTo(before.size() + 1));
+      assertThat(after, equalTo(
+              before.withAdded(group.withtId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
 
 
   }
 
-  @Test
+  @Test (enabled = false)
   public void testBadGroupCreation() throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
